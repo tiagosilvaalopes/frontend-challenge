55a4074ba68a43b19168c20263164253c5cf7f39
Tiago Lopes <tiago.silvaalopes@gmail.com>
2024-01-21 17:06:26 +0000

    Added NavBar component

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import FetchDataIcon from '@/assets/icons/IconFetchData.vue'
+import UploadIcon from '@/assets/icons/IconUpload.vue'
+import { useTranscriptionsStore } from '@/stores/transcriptions'
+
+const transcriptionsStore = useTranscriptionsStore()
+const { fetchTranscriptions, uploadTranscriptions } = transcriptionsStore
+</script>
+
+<template>
+  <header class="navbar">
+    <div class="navbar-wrapper">
+      <div class="title">Transcriptions</div>
+      <div>
+        <button class="btn" @click="uploadTranscriptions"><UploadIcon /></button>
+        <button class="btn" @click="fetchTranscriptions"><FetchDataIcon /></button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  width: 100%;
+  height: 70px;
+  border-bottom: 2px solid #c0c5cf;
+  background-color: white;
+  margin-bottom: 20px;
+}
+
+.navbar-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 700px;
+  width: 700px;
+  padding: 0 10px;
+}
+
+.title {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 600;
+  font-size: 1.125rem;
+  color: #414c5e;
+}
+
+.btn {
+  margin-left: 20px;
+}
+</style>
